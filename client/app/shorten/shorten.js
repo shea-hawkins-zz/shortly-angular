f88c202463f6b101b097aedf05e8ace6fd225a03
Hack Reactor Students <accounts@hackreactor.com>
2016-07-08 18:56:17 -0700

    Add URL input form.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,8 +1,15 @@
+
+
 angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
+  var rValidUrl = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
   $scope.link = {};
+  $scope.validUrl = true;
   $scope.addLink = function() {
     Links.addOne($scope.link);
+  };
+  $scope.isValidUrl = function (url) {
+    $scope.validUrl = url.match(rValidUrl);
   };
 });
