9c42639fb5463b848fb0f44c2354e4dc8f5af670
Hack Reactor Students <accounts@hackreactor.com>
2016-07-08 20:57:08 -0700

    Add sort and filter to link table

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -2,6 +2,7 @@ angular.module('shortly.links', [])
 
 .controller('LinksController', function ($scope, Links) {
   $scope.data = {};
+  $scope.criteria;
   Links.getAll().then(function(links) {
     $scope.data.links = links;
   });
