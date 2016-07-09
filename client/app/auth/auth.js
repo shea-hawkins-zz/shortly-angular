c4181c1465c18f9123951753538de43a2899d928
Hack Reactor Students <accounts@hackreactor.com>
2016-07-08 20:12:52 -0700

    Validate username and password

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -3,28 +3,40 @@
 // in our signup/signin forms using the injected Auth service
 angular.module('shortly.auth', [])
 
-.controller('AuthController', function ($scope, $window, $location, Auth) {
+.controller('AuthController', function ($scope, $window, $location, Auth, Validation) {
   $scope.user = {};
+  $scope.failed = {
+    username: [],
+    password: []
+  };
+
+  $scope.validate = function(string, type) {
+    $scope.failed[type] = Validation.validate(string, type);
+  };
 
   $scope.signin = function () {
-    Auth.signin($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    if (!$scope.failed.username.length && !$scope.failed.password.length) {
+      Auth.signin($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
   };
 
   $scope.signup = function () {
-    Auth.signup($scope.user)
-      .then(function (token) {
-        $window.localStorage.setItem('com.shortly', token);
-        $location.path('/links');
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+    if (!$scope.failed.username.length && !$scope.failed.password.length) {
+      Auth.signup($scope.user)
+        .then(function (token) {
+          $window.localStorage.setItem('com.shortly', token);
+          $location.path('/links');
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
   };
 });
